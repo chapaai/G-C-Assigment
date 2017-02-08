439906ed548d39ae94f4d9dcbf505e1c277b7080
chapaai <utku.demirhan@gmail.com>
2017-02-08 23:48:00 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,47 +1,43 @@
-## firstly we read our train and test data into variables.
+## firstly we read our datas into variables. Assuming your are in the same folder with all those text files.
 
 train <- read.table("X_train.txt")
 test <- read.table("X_test.txt")
+subject_test <- read.table("subject_test.txt")
+subject_train <- read.table("subject_train.txt")
+y_train <- read.table("y_train.txt")
+y_test <- read.table("y_test.txt")
 
-dim(train) ## dim gives 561 variables
+## we read into features object only the second column. So we dont have 1,2,3 rows in the data.
+features <- read.table("features.txt", colClasses = c("NULL", NA))
 
-dim(test) ## dim gives 561 variables. Equal to train data set
+## takes the column names into the deneme object
+deneme <- as.character(features$V2) 
 
-
-features <- read.table("features.txt", colClasses = c("NULL", NA))## we read into features object only the second column. So we dont have 1,2,3 rows in the data.
-
-deneme <- as.character(features$V2) ## takes the column names into the deneme object
-
-colnames(test) <- deneme ## adds columnnames to test and train objects. Now we have variable names on top of the values.
+## adds columnnames to test and train objects. Now we have variable names on top of the values.
+colnames(test) <- deneme 
 colnames(train) <- deneme
 
-a <- bind_cols(y_train, train) ## merged y_train and train data 
+## merged y_train, subject_train and train data 
+a <- bind_cols(y_train, subject_train, train) 
 
-
-a$V1[a$V1 == "1"] <- "WALKING"; a$V1[a$V1 == "2"] <- "WALKING_UPSTAIRS"; a$V1[a$V1 == "3"] <- "WALKING_DOWNSTAIRS"; a$V1[a$V1 == "4"] <- "SITTING"; a$V1[a$V1 == "5"] <- "STANDING"; a$V1[a$V1 == "6"] <- "LAYING";
-
-## then changed 1,2,3,4,5,6 values of data to text (Walking, Sitting etc.)
-
-b <- bind_cols(y_test, test) ## merged y_test and test data
-
-b$V1[b$V1 == "1"] <- "WALKING"; b$V1[b$V1 == "2"] <- "WALKING_UPSTAIRS"; b$V1[b$V1 == "3"] <- "WALKING_DOWNSTAIRS"; b$V1[b$V1 == "4"] <- "SITTING"; b$V1[b$V1 == "5"] <- "STANDING"; b$V1[b$V1 == "6"] <- "LAYING";
+## changed the first and second colname
+colnames(a)[1] <- c("Activities") 
+colnames(a)[2] <- c("Subjects")
 
 ## then changed 1,2,3,4,5,6 values of data to text (Walking, Sitting etc.)
+a$Activities[a$Activities == "1"] <- "WALKING"; a$Activities[a$Activities == "2"] <- "WALKING_UPSTAIRS"; a$Activities[a$Activities == "3"] <- "WALKING_DOWNSTAIRS"; a$Activities[a$Activities == "4"] <- "SITTING"; a$Activities[a$Activities == "5"] <- "STANDING"; a$Activities[a$Activities == "6"] <- "LAYING";
 
-rbind(train, test) ## binds two datasets together. 1st step is done.
+## same here for "test" 
+b <- bind_cols(y_test, subject_test, test) ## merged y_test and test data
+colnames(b)[1] <- c("Activities") ## changed the first colname
+colnames(b)[2] <- c("Subjects")
+b$Activities[b$Activities == "1"] <- "WALKING"; b$Activities[b$Activities == "2"] <- "WALKING_UPSTAIRS"; b$Activities[b$Activities == "3"] <- "WALKING_DOWNSTAIRS"; b$Activities[b$Activities == "4"] <- "SITTING"; b$Activities[b$Activities == "5"] <- "STANDING"; b$Activities[b$Activities == "6"] <- "LAYING";
+ 
+## binds two datasets together.
+bindedtable <- rbind(a, b)
 
-meanandstd <- bindedtable[grep("mean|std|V1", names(bindedtable))] ## with grep we searched for mean and std keywords inside the names of the bindedtable object.
+## with grep we searched for mean and std keywords inside the names of the bindedtable object. Also didnt throw away activity and subject too.
+meanandstd <- bindedtable[grep("mean|std|Activity|Subjects", names(bindedtable))] 
 
-## then subsetted the bindedtable object according to the grepped values. 2nd step is done.
-
-colnames(meanandstd)[1] <- c("Activities") ## changed the first colname 
-
-## via purr package i have been able to take the average of each variable.
-
-install.packages("purrr")
-
-library("purrr")
-
-meanandstd2 <- meanandstd %>% slice_rows("Activities") %>% dmap(mean)
-
-View(meanandstd2)
+## Aggregate gives the mean off other data than activities and subject, grouped by activities and subject. 
+second_data <- aggregate(. ~ Activities + Subjects, data = meanandstd, FUN = 'mean')
